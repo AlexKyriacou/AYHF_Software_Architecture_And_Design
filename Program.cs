8fd50fca697e939851b94c803a59b28c5b2f2388
AlexKyriacou <92493791+AlexKyriacou@users.noreply.github.com>
2023-05-16 15:47:57 +1000

    Added Database File To Be Used By Repository Classes

--- Program.cs
+++ Program.cs
@@ -1,6 +1,29 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+using System;
+using System.Data.SQLite;
 
-app.MapGet("/", () => "Hello World!");
+class Program
+{
+    static void Main()
+    {
+        // Set the path to your SQLite database file
+        string databasePath = "C:\\Uni\\Software Architecture & Design\\AYHF\\AYHF_Software_Architecture_And_Design\\src\\database.db";
 
-app.Run();
+        // Create a SQLite connection
+        using (SQLiteConnection connection = CreateSQLiteConnection(databasePath))
+        {
+            // Open the connection
+            connection.Open();
+
+
+            // Close the connection
+            connection.Close();
+        }
+    }
+
+    static SQLiteConnection CreateSQLiteConnection(string databasePath)
+    {
+        string connectionString = $"Data Source={databasePath};Version=3;";
+        SQLiteConnection connection = new SQLiteConnection(connectionString);
+        return connection;
+    }
+}
