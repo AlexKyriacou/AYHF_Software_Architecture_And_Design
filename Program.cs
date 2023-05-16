62a9c9649e12f5720f87a237d901eeabc79c1d65
AlexKyriacou <92493791+AlexKyriacou@users.noreply.github.com>
2023-05-16 16:05:19 +1000

    Revert "Added Database File To Be Used By Repository Classes"
    This reverts commit 8fd50fca697e939851b94c803a59b28c5b2f2388.

--- Program.cs
+++ Program.cs
@@ -1,29 +1,6 @@
-using System;
-using System.Data.SQLite;
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
 
-class Program
-{
-    static void Main()
-    {
-        // Set the path to your SQLite database file
-        string databasePath = "C:\\Uni\\Software Architecture & Design\\AYHF\\AYHF_Software_Architecture_And_Design\\src\\database.db";
+app.MapGet("/", () => "Hello World!");
 
-        // Create a SQLite connection
-        using (SQLiteConnection connection = CreateSQLiteConnection(databasePath))
-        {
-            // Open the connection
-            connection.Open();
-
-
-            // Close the connection
-            connection.Close();
-        }
-    }
-
-    static SQLiteConnection CreateSQLiteConnection(string databasePath)
-    {
-        string connectionString = $"Data Source={databasePath};Version=3;";
-        SQLiteConnection connection = new SQLiteConnection(connectionString);
-        return connection;
-    }
-}
+app.Run();
