7badb4f6105f58e41ab7c54b7700e72feb4c2d6f
Julian Codespoti <julian.codespoti@myob.com>
2023-05-16 15:59:33 +1000

    Create template home page and login page with navbar

--- WebUI/all-your-healthy-foods/src/App.js
+++ WebUI/all-your-healthy-foods/src/App.js
@@ -1,10 +1,21 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
+import LoginPage from './Pages/LoginPage';
 import './App.css';
 
 function App() {
   return (
-    <div>
-      <h1 className="Header">All Your Healthy Foods</h1>
-    </div>
+    <Router>
+      <div className="App">
+        <nav className="navbar">
+          <a href='/' className="App-header">All Your Healthy Foods</a>
+          <Link className="login-button" to="/login">Login</Link>
+        </nav>
+        <Routes>
+          <Route path="/login" element={<LoginPage />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
