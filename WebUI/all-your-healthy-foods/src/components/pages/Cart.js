1d0d43267848c0800d90ebb7536271f0bc3dda52
Marella Morad <marella99mourad@gmail.com>
2023-05-21 22:25:22 +1000

    Add Cart styling

--- WebUI/all-your-healthy-foods/src/components/pages/Cart.js
+++ WebUI/all-your-healthy-foods/src/components/pages/Cart.js
@@ -5,17 +5,26 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTrash, faMinus, faPlus } from '@fortawesome/free-solid-svg-icons';
 import './Cart.css';
 
-function calculateTotal(cartItems) {
-    let total = 0;
+function calculateTotal(cartItems, promotionPercentage) {
+    let subtotal = 0;
     for (const item of cartItems) {
-        total += parseFloat(item.price) * item.count;
+        subtotal += parseFloat(item.price) * item.count;
     }
-    return total.toFixed(2);
+
+    const promotionAmount = subtotal * (promotionPercentage / 100);
+    const total = subtotal - promotionAmount;
+
+    return {
+        subtotal: subtotal.toFixed(2),
+        promotionAmount: promotionAmount.toFixed(2),
+        total: total.toFixed(2)
+    };
 }
 
 function Cart() {
     const { cartItems, removeFromCart, increaseCount, decreaseCount } = useContext(CartContext);
-    const total = calculateTotal(Object.values(cartItems));
+    const promotionPercentage = 10; // Assuming a 10% promotion for this example
+    const { subtotal, promotionAmount, total } = calculateTotal(Object.values(cartItems), promotionPercentage);
 
     const handleRemoveItem = (item) => {
         removeFromCart(item);
@@ -42,51 +51,79 @@ function Cart() {
         return grouped;
     }, {});
 
+    const sortedGroupedProducts = Object.fromEntries(
+        Object.entries(groupedProducts).sort(([nameA], [nameB]) => nameA.localeCompare(nameB))
+    );
+
     return (
-        <div>
+        <div className="cart">
             <h2>Your Cart</h2>
-            {Object.keys(groupedProducts).length === 0 ? (
-                <p>Your cart is empty.</p>
+            {Object.keys(sortedGroupedProducts).length === 0 ? (
+                <div>
+                    <p>Your cart is empty.</p>
+                    <Link className="back-to-home" to="/">Continue Shopping</Link>
+                </div>
             ) : (
                 <ul>
-                    {Object.entries(groupedProducts).map(([name, item], index) => (
+                    {Object.entries(sortedGroupedProducts).map(([name, item], index) => (
                         <li key={index}>
-                            <div>
+                            <div className="item-container">
                                 <img src={item.image} alt={item.name} />
-                                <div>
-                                    <p>{name} - {item.description}</p>
-                                    <div className="count-buttons">
-                                        <button
-                                            className="count-button"
-                                            onClick={() => handleDecreaseCount(item.name)}
-                                            disabled={item.count === 1}
-                                        >
-                                            <FontAwesomeIcon icon={faMinus} />
-                                        </button>
-                                        <span className="count">{item.count}</span>
-                                        <button className="count-button" onClick={() => handleIncreaseCount(item.name)}>
-                                            <FontAwesomeIcon icon={faPlus} />
-                                        </button>
-                                    </div>
-                                    <button className='remove-button' onClick={() => handleRemoveItem(item)}>
-                                        <FontAwesomeIcon className='shopping-list' icon={faTrash} />
-                                        Remove
+                                <p><strong>{name}</strong> - {item.description}</p>
+                                <div className="count-buttons">
+                                    <button
+                                        className="count-button"
+                                        onClick={() => handleDecreaseCount(item.name)}
+                                        disabled={item.count === 1}
+                                    >
+                                        <FontAwesomeIcon icon={faMinus} />
+                                    </button>
+                                    <span className="count">{item.count}</span>
+                                    <button className="count-button" onClick={() => handleIncreaseCount(item.name)}>
+                                        <FontAwesomeIcon icon={faPlus} />
                                     </button>
                                 </div>
-                                <div>$ {item.price}</div>
+                                <button className="remove-button" onClick={() => handleRemoveItem(item)}>
+                                    <FontAwesomeIcon className="shopping-list" icon={faTrash} />
+                                    Remove
+                                </button>
+                                <div>$ {(item.price * item.count).toFixed(2)}</div>
                             </div>
                         </li>
                     ))}
                 </ul>
             )}
-            <div className='summary-card'>
-                <p>Subtotal</p>
-                <p>Promotion</p>
-                <hr></hr>
-                <p>Total {total}</p>
-                <button className='checkout-button'>Checkout</button>
-                <Link className='back-to-home' to="/">Continue Shopping</Link>
-            </div>
+            {Object.keys(sortedGroupedProducts).length === 0 ? null : (
+                <div className="summary-card">
+                    <div className="summary-row">
+                        <span>Item Breakdown</span>
+                    </div>
+                    {Object.entries(sortedGroupedProducts).map(([name, item]) => (
+                        <div key={name} className="summary-row">
+                            <span className="item-name">{name}</span>
+                            <span className="item-count">x {item.count}</span>
+                            <span className="item-price">${(item.price * item.count).toFixed(2)}</span>
+                        </div>
+                    ))}
+                    <hr />
+                    <div className="summary-row">
+                        <span>Subtotal</span>
+                        <span className="price">${subtotal}</span>
+                    </div>
+                    <div className="summary-row">
+                        <span>Promotion</span>
+                        <span className="price">- ${promotionAmount}</span>
+                    </div>
+                    <hr />
+                    <div className="summary-row">
+                        <span>Total</span>
+                        <span className="price">${total}</span>
+                    </div>
+
+                    <button className="checkout-button">Checkout</button>
+                    <Link className="back-to-home" to="/">Continue Shopping</Link>
+                </div>
+            )}
         </div>
     );
 }
