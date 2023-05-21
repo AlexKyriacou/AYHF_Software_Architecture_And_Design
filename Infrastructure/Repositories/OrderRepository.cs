d1357e405bf3ecaa30b70eee2c1508e6f75b9a19
AlexKyriacou <92493791+AlexKyriacou@users.noreply.github.com>
2023-05-21 16:08:39 +1000

    Implemented Draft Order Repo & Product Repo

--- Infrastructure/Repositories/OrderRepository.cs
+++ Infrastructure/Repositories/OrderRepository.cs
@@ -1,40 +1,234 @@
+using Microsoft.Data.Sqlite;
 using MyProject.Domain.Models;
 
 namespace MyProject.Infrastructure.Repositories;
 
-public class OrderRepository
+public class OrderRepository : RepositoryBase
 {
-    private readonly List<Order> _orders;
+    private static OrderRepository? _instance;
 
     public OrderRepository()
     {
-        _orders = new List<Order>();
+        _instance = this;
+    }
+
+    public static OrderRepository Instance
+    {
+        get
+        {
+            _instance ??= new OrderRepository();
+            return _instance;
+        }
     }
 
     public void AddOrder(Order order)
     {
-        _orders.Add(order);
+        CreateTables();
+
+        using (var transaction = Connection.BeginTransaction())
+        {
+            var command = Connection.CreateCommand();
+            command.CommandText = "INSERT INTO Orders (OrderId, CustomerId, OrderDate, TotalAmount, IsCompleted) " +
+                                  "VALUES (@orderId, @customerId, @orderDate, @totalAmount, @isCompleted)";
+            command.Parameters.AddWithValue("@orderId", order.Id);
+            command.Parameters.AddWithValue("@customerId", order.Customer.Id);
+            command.Parameters.AddWithValue("@orderDate", order.OrderDate);
+            command.Parameters.AddWithValue("@totalAmount", order.TotalAmount);
+            command.Parameters.AddWithValue("@isCompleted", order.IsCompleted ? 1 : 0);
+
+            command.ExecuteNonQuery();
+
+            // Save the products for the order
+            SaveOrderProducts(order);
+
+            transaction.Commit();
+        }
     }
 
     public void UpdateOrder(Order order)
     {
-        // Update the order in the database
+        CreateTables();
+
+        using (var transaction = Connection.BeginTransaction())
+        {
+            var command = Connection.CreateCommand();
+            command.CommandText = "UPDATE Orders SET CustomerId = @customerId, OrderDate = @orderDate, " +
+                                  "TotalAmount = @totalAmount, IsCompleted = @isCompleted WHERE OrderId = @orderId";
+            command.Parameters.AddWithValue("@customerId", order.Customer.Id);
+            command.Parameters.AddWithValue("@orderDate", order.OrderDate);
+            command.Parameters.AddWithValue("@totalAmount", order.TotalAmount);
+            command.Parameters.AddWithValue("@isCompleted", order.IsCompleted ? 1 : 0);
+            command.Parameters.AddWithValue("@orderId", order.Id);
+
+            command.ExecuteNonQuery();
+
+            // Save the updated products for the order
+            SaveOrderProducts(order);
+
+            transaction.Commit();
+        }
     }
 
     public void DeleteOrder(Order order)
     {
-        _orders.Remove(order);
+        CreateTables();
+
+        using (var transaction = Connection.BeginTransaction())
+        {
+            var command = Connection.CreateCommand();
+            command.CommandText = "DELETE FROM Orders WHERE OrderId = @orderId";
+            command.Parameters.AddWithValue("@orderId", order.Id);
+
+            command.ExecuteNonQuery();
+
+            transaction.Commit();
+        }
     }
 
-    public Order GetOrderById(int orderId)
+
+    public Order? GetOrderById(int orderId)
     {
-        // Retrieve the order from the database by ID
-        return _orders.Find(o => o.Id == orderId);
+        CreateTables();
+
+        using (var command = new SqliteCommand("SELECT * FROM Orders WHERE OrderId = @orderId", Connection))
+        {
+            command.Parameters.AddWithValue("@orderId", orderId);
+
+            using (var reader = command.ExecuteReader())
+            {
+                if (reader.Read())
+                {
+                    Customer? customer = UserRepository.Instance.GetUserById<Customer>(reader.GetInt32(1));
+                    if (customer != null)
+                    {
+                        List<Product> products = GetOrderProducts(orderId);
+
+                        Order order = new(customer, products)
+                        {
+                            Id = reader.GetInt32(0),
+                            OrderDate = reader.GetDateTime(3),
+                            TotalAmount = reader.GetDecimal(4),
+                            IsCompleted = reader.GetBoolean(5)
+                        };
+
+                        return order;
+                    }
+                }
+            }
+        }
+
+        return null; // Return null if the order with the given ID is not found
     }
 
     public List<Order> GetAllOrders()
     {
-        // Retrieve all orders from the database
-        return _orders;
+        CreateTables();
+
+        List<Order> orders = new List<Order>();
+
+        using (var command = new SqliteCommand("SELECT * FROM Orders", Connection))
+        {
+            using (var reader = command.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    Customer? customer = UserRepository.Instance.GetUserById<Customer>(reader.GetInt32(1));
+                    if (customer != null)
+                    {
+                        List<Product> products = GetOrderProducts(reader.GetInt32(0));
+
+                        Order order = new(customer, products)
+                        {
+                            Id = reader.GetInt32(0),
+                            OrderDate = reader.GetDateTime(3),
+                            TotalAmount = reader.GetDecimal(4),
+                            IsCompleted = reader.GetBoolean(5)
+                        };
+
+                        orders.Add(order);
+                    }
+                }
+            }
+        }
+
+        return orders;
+    }
+
+    private void SaveOrderProducts(Order order)
+    {
+        // Delete existing order products
+        using (var command = Connection.CreateCommand())
+        {
+            command.CommandText = "DELETE FROM OrderProducts WHERE OrderId = @orderId";
+            command.Parameters.AddWithValue("@orderId", order.Id);
+
+            command.ExecuteNonQuery();
+        }
+
+        // Insert new order products
+        using (var command = Connection.CreateCommand())
+        {
+            command.CommandText = "INSERT INTO OrderProducts (OrderId, ProductId) VALUES (@orderId, @productId)";
+
+            foreach (var product in order.Products)
+            {
+                command.Parameters.Clear();
+                command.Parameters.AddWithValue("@orderId", order.Id);
+                command.Parameters.AddWithValue("@productId", product.Id);
+
+                command.ExecuteNonQuery();
+            }
+        }
+    }
+
+    private List<Product> GetOrderProducts(int orderId)
+    {
+        List<Product> products = new();
+
+        using (var command = new SqliteCommand("SELECT * FROM OrderProducts WHERE OrderId = @orderId", Connection))
+        {
+            command.Parameters.AddWithValue("@orderId", orderId);
+
+            using (var reader = command.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    int productId = reader.GetInt32(1);
+                    Product? product = ProductRepository.Instance.GetProductById(productId);
+                    if (product != null)
+                    {
+                        products.Add(product);
+                    }
+                }
+            }
+        }
+
+        return products;
+    }
+
+    protected override void CreateTables()
+    {
+        // Create the Orders table if it doesn't exist
+        using (var command = Connection.CreateCommand())
+        {
+            command.CommandText = "CREATE TABLE IF NOT EXISTS Orders (" +
+                                  "OrderId INTEGER PRIMARY KEY, " +
+                                  "CustomerId INTEGER, " +
+                                  "OrderDate TEXT, " +
+                                  "TotalAmount DECIMAL(10, 2), " +
+                                  "IsCompleted INTEGER)";
+            command.ExecuteNonQuery();
+        }
+
+        // Create the OrderProducts table if it doesn't exist
+        using (var command = Connection.CreateCommand())
+        {
+            command.CommandText = "CREATE TABLE IF NOT EXISTS OrderProducts (" +
+                                  "OrderId INTEGER, " +
+                                  "ProductId INTEGER, " +
+                                  "FOREIGN KEY(OrderId) REFERENCES Orders(OrderId), " +
+                                  "FOREIGN KEY(ProductId) REFERENCES Products(ProductId))";
+            command.ExecuteNonQuery();
+        }
     }
 }
--- Infrastructure/Repositories/ProductRepository.cs
+++ Infrastructure/Repositories/ProductRepository.cs
@@ -1,40 +1,151 @@
+using Microsoft.Data.Sqlite;
 using MyProject.Domain.Models;
 
 namespace MyProject.Infrastructure.Repositories;
 
-public class ProductRepository
+public class ProductRepository : RepositoryBase
 {
-    private readonly List<Product> _productList;
+    private static ProductRepository? _instance;
 
     public ProductRepository()
     {
-        _productList = new List<Product>();
+        _instance = this;
+    }
+
+    public static ProductRepository Instance
+    {
+        get
+        {
+            _instance ??= new ProductRepository();
+            return _instance;
+        }
     }
 
     public void AddProduct(Product product)
     {
-        _productList.Add(product);
+        CreateTables();
+
+        using (var transaction = Connection.BeginTransaction())
+        {
+            var command = Connection.CreateCommand();
+            command.CommandText = "INSERT INTO Products (Id, Name, Price, Description, Images) VALUES (@id, @name, @price, @description, @images)";
+            command.Parameters.AddWithValue("@id", product.Id);
+            command.Parameters.AddWithValue("@name", product.Name);
+            command.Parameters.AddWithValue("@price", product.Price);
+            command.Parameters.AddWithValue("@description", product.Description);
+            command.Parameters.AddWithValue("@images", string.Join(",", product.Images));
+
+            command.ExecuteNonQuery();
+
+            transaction.Commit();
+        }
     }
 
     public void UpdateProduct(Product product)
     {
-        // Update the product in the database
+        CreateTables();
+
+        using (var transaction = Connection.BeginTransaction())
+        {
+            var command = Connection.CreateCommand();
+            command.CommandText = "UPDATE Products SET Name = @name, Price = @price, Description = @description, Images = @images WHERE Id = @id";
+            command.Parameters.AddWithValue("@name", product.Name);
+            command.Parameters.AddWithValue("@price", product.Price);
+            command.Parameters.AddWithValue("@description", product.Description);
+            command.Parameters.AddWithValue("@images", string.Join(",", product.Images));
+            command.Parameters.AddWithValue("@id", product.Id);
+
+            command.ExecuteNonQuery();
+
+            transaction.Commit();
+        }
     }
 
     public void DeleteProduct(Product product)
     {
-        _productList.Remove(product);
+        CreateTables();
+
+        using (var transaction = Connection.BeginTransaction())
+        {
+            var command = Connection.CreateCommand();
+            command.CommandText = "DELETE FROM Products WHERE Id = @id";
+            command.Parameters.AddWithValue("@id", product.Id);
+
+            command.ExecuteNonQuery();
+
+            transaction.Commit();
+        }
     }
 
-    public Product GetProductById(int productId)
+    public Product? GetProductById(int productId)
     {
-        // Retrieve the product from the database by ID
-        return _productList.Find(p => p.Id == productId);
+        CreateTables();
+
+        string selectQuery = "SELECT * FROM Products WHERE Id = @productId";
+        using (var selectCommand = new SqliteCommand(selectQuery, Connection))
+        {
+            selectCommand.Parameters.AddWithValue("@productId", productId);
+
+            using (var reader = selectCommand.ExecuteReader())
+            {
+                if (reader.Read())
+                {
+                    Product product = new Product(
+                        id: reader.GetInt32(0),
+                        name: reader.GetString(1),
+                        price: reader.GetDecimal(2),
+                        description: reader.GetString(3),
+                        images: reader.GetString(4).Split(','));
+
+                    return product;
+                }
+            }
+        }
+
+        return null; // Return null if the product with the given ID is not found
     }
 
     public List<Product> GetAllProducts()
     {
-        // Retrieve all products from the database
-        return _productList;
+        CreateTables();
+
+        List<Product> products = new List<Product>();
+
+        string selectQuery = "SELECT * FROM Products";
+        using (var selectCommand = new SqliteCommand(selectQuery, Connection))
+        {
+            using (var reader = selectCommand.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    Product product = new Product(
+                        id: reader.GetInt32(0),
+                        name: reader.GetString(1),
+                        price: reader.GetDecimal(2),
+                        description: reader.GetString(3),
+                        images: reader.GetString(4).Split(','));
+
+                    products.Add(product);
+                }
+            }
+        }
+
+        return products;
+    }
+
+    protected override void CreateTables()
+    {
+        string createTableQuery = @"
+                    CREATE TABLE IF NOT EXISTS Products (
+                        Id INTEGER PRIMARY KEY AUTOINCREMENT,
+                        Name TEXT NOT NULL,
+                        Price REAL NOT NULL,
+                        Description TEXT,
+                        Images TEXT
+                    )";
+        using (var createTableCommand = new SqliteCommand(createTableQuery, Connection))
+        {
+            createTableCommand.ExecuteNonQuery();
+        }
     }
 }
