5d4d55890c9d563548e674df05860152e96382a7
Marella Morad <marella99mourad@gmail.com>
2023-05-22 18:58:35 +1000

    Add cart memory

--- WebUI/all-your-healthy-foods/src/pages/cart/CartContext.js
+++ WebUI/all-your-healthy-foods/src/pages/cart/CartContext.js
@@ -1,4 +1,4 @@
-import React, { createContext, useState } from 'react';
+import React, { createContext, useState, useEffect } from 'react';
 
 const CartContext = createContext();
 
@@ -6,16 +6,31 @@ const CartProvider = ({ children }) => {
     const [cartCount, setCartCount] = useState(0);
     const [cartItems, setCartItems] = useState([]);
 
+    useEffect(() => {
+        // Load cart items from local storage when the component mounts
+        const storedCartItems = localStorage.getItem('cartItems');
+        if (storedCartItems) {
+            setCartItems(JSON.parse(storedCartItems));
+            setCartCount(JSON.parse(storedCartItems).length);
+        }
+    }, []);
+
+    const updateCartItems = (updatedItems) => {
+        setCartItems(updatedItems);
+        setCartCount(updatedItems.length);
+        localStorage.setItem('cartItems', JSON.stringify(updatedItems));
+    };
+
     const addToCart = (product) => {
         const updatedCartItems = [...cartItems, { ...product, count: 1 }];
-        setCartItems(updatedCartItems);
-        setCartCount(cartCount + 1);
+        updateCartItems(updatedCartItems);
     };
 
     const removeFromCart = (item) => {
-        const updatedCartItems = cartItems.filter((cartItem) => cartItem.name !== item.name);
-        setCartItems(updatedCartItems);
-        setCartCount(cartCount - 1);
+        const updatedCartItems = cartItems.filter(
+            (cartItem) => cartItem.name !== item.name
+        );
+        updateCartItems(updatedCartItems);
     };
 
     const increaseCount = (itemName) => {
@@ -33,6 +48,7 @@ const CartProvider = ({ children }) => {
             updatedCartItems.splice(foundItemIndex, 1);
             setCartItems(updatedCartItems);
             setCartCount(cartCount - 1);
+            updateCartItems(updatedCartItems);
         }
     };
 
