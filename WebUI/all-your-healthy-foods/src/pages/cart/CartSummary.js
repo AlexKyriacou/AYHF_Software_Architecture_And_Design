88afec7fed454a5773d73fec3b93e20bf532a0a6
Marella Morad <marella99mourad@gmail.com>
2023-05-22 23:12:06 +1000

    Make CartSummary reusable

---
+++ WebUI/all-your-healthy-foods/src/pages/cart/CartSummary.js
@@ -0,0 +1,57 @@
+import React, { useContext } from 'react';
+import { CartContext } from './CartContext';
+import './Cart.css';
+
+function calculateTotal(cartItems, promotionPercentage) {
+    let subtotal = 0;
+    for (const item of cartItems) {
+        subtotal += parseFloat(item.price) * item.count;
+    }
+
+    const promotionAmount = subtotal * (promotionPercentage / 100);
+    const total = subtotal - promotionAmount;
+
+    return {
+        subtotal: subtotal.toFixed(2),
+        promotionAmount: promotionAmount.toFixed(2),
+        total: total.toFixed(2)
+    };
+}
+
+function CartSummary({ extra }) {
+    const { cartItems, sortedGroupedProducts } = useContext(CartContext);
+    const promotionPercentage = 10; // Assuming a 10% promotion for this example
+    const { subtotal, promotionAmount, total } = calculateTotal(Object.values(cartItems), promotionPercentage);
+
+    return (
+        <div className="summary-card">
+            <div className="summary-row">
+                <span>Item Breakdown</span>
+            </div>
+            {Object.entries(sortedGroupedProducts).map(([name, item]) => (
+                <div key={name} className="summary-row">
+                    <span className="item-name">{name}</span>
+                    <span className="item-count">x {item.count}</span>
+                    <span className="item-price">${(item.price * item.count).toFixed(2)}</span>
+                </div>
+            ))}
+            <hr />
+            <div className="summary-row">
+                <span>Subtotal</span>
+                <span className="price">${subtotal}</span>
+            </div>
+            <div className="summary-row">
+                <span>Promotion</span>
+                <span className="price">- ${promotionAmount}</span>
+            </div>
+            <hr />
+            <div className="summary-row">
+                <span>Total</span>
+                <span className="price">${total}</span>
+            </div>
+            <span className="extra">{extra}</span>
+        </div>
+    );
+}
+
+export default CartSummary;
