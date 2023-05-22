97166f14053db21b960e79477e367dbe488f3094
Marella Morad <marella99mourad@gmail.com>
2023-05-22 22:58:48 +1000

    Make login and signup almost fully functional

--- WebUI/all-your-healthy-foods/src/components/login/LoginPage.js
+++ WebUI/all-your-healthy-foods/src/components/login/LoginPage.js
@@ -1,16 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Login.css';
 import { Link } from 'react-router-dom';
 import PasswordInput from "./PasswordInput";
 
 const LoginPage = () => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleUsernameChange = (event) => {
+        setUsername(event.target.value);
+    };
+
+    const handlePasswordChange = (event) => {
+        setPassword(event.target.value);
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        const user = {
+            username,
+            password
+        };
+
+        // TODO: Send the user object to the backend and get the backend response
+        // (including whether the user exists, and if so what's their role)
+        console.log("User: ", user);
+    };
+
     return (
         <div className="login-container">
             <div className="login-card">
                 <h2 className="login-title">Login</h2>
-                <form className="login-form">
-                    <input type="text" placeholder="Username" className="login-input" required />
-                    <PasswordInput placeholder="Password" />
+                <form className="login-form" onSubmit={handleSubmit}>
+                    <input
+                        type="text"
+                        placeholder="Username"
+                        className="login-input"
+                        value={username}
+                        onChange={handleUsernameChange}
+                        required
+                    />
+                    <PasswordInput
+                        placeholder="Password"
+                        value={password}
+                        onChange={handlePasswordChange}
+                        clear={() => setPassword('')}
+                    />
                     <button type="submit" className="login-button">Login</button>
                 </form>
                 <Link className='switch' to='/signup'>Not Already a member? Sign Up Now!</Link>
--- WebUI/all-your-healthy-foods/src/components/login/SignupPage.js
+++ WebUI/all-your-healthy-foods/src/components/login/SignupPage.js
@@ -1,15 +1,16 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./Login.css";
 import { Link } from "react-router-dom";
 import PasswordInput from "./PasswordInput";
 
 const SignupPage = () => {
-    // const [isAdmin, setIsAdmin] = useState(false);
-    // const handleAdminCheckboxChange = (event) => {
-    //     setIsAdmin(event.target.checked);
-    // };
-
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [username, setUsername] = useState("");
+    const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const [confirmPassword, setConfirmPassword] = useState("");
+    const [role, setRole] = useState("");
 
     const [passwordError, setPasswordError] = useState("");
 
@@ -18,11 +19,22 @@ const SignupPage = () => {
         return passwordValidationRegex.test(password);
     };
 
-    const [confirmPassword, setConfirmPassword] = useState("");
-
     const doPasswordsMatch = (password, confirmPassword) => {
         return password === confirmPassword;
     };
+
+    useEffect(() => {
+        const generateUsername = () => {
+            if (firstName.trim() === "" && lastName.trim() === "") {
+                setUsername("");
+            } else {
+                const generatedUsername = `${firstName}.${lastName}`;
+                setUsername(generatedUsername);
+            }
+        };
+
+        generateUsername();
+    }, [firstName, lastName]);
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -30,7 +42,16 @@ const SignupPage = () => {
         if (isValidPassword(password)) {
             if (doPasswordsMatch(password, confirmPassword)) {
                 setPasswordError("");
-                // Proceed with form submission
+
+                const user = {
+                    username,
+                    email,
+                    password,
+                    role
+                };
+
+                // TODO: Send the user object to the backend and get the backend response
+                console.log("User: ", user);
             } else {
                 setPasswordError("Passwords do not match");
             }
@@ -44,27 +65,69 @@ const SignupPage = () => {
             <div className="signup-card">
                 <h2 className="signup-title">Create Account</h2>
                 <form className="signup-form" onSubmit={handleSubmit}>
+                    <input
+                        type="text"
+                        placeholder="Username"
+                        className="signup-input readonly"
+                        required
+                        value={username}
+                        readOnly
+                    />
                     <div className="signup-input-group">
-                        <input type="text" placeholder="First name" className="signup-input" required />
-                        <span className="input-space"></span>
-                        <input type="text" placeholder="Last Name" className="signup-input" required />
+                        <input
+                            type="text"
+                            placeholder="First name"
+                            className="signup-input"
+                            required
+                            value={firstName}
+                            onChange={(e) => setFirstName(e.target.value)}
+                        />
+                        <input
+                            type="text"
+                            placeholder="Last Name"
+                            className="signup-input"
+                            required
+                            value={lastName}
+                            onChange={(e) => setLastName(e.target.value)}
+                        />
                     </div>
-                    <input type="text" placeholder="Mobile Number" className="signup-input" required />
-                    <input type="email" placeholder="Email" className="signup-input" required />
-                    <PasswordInput placeholder="Password" showInfo={true} onChange={(e) => setPassword(e.target.value)} passwordError={passwordError} />
-                    <PasswordInput placeholder="Confirm password" value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)} passwordError={passwordError} />
-                    {
-                        //Commenting this out until we come up with a better solution
-                        /* <div className="admin-checkbox-container">
-                            <input
-                                type="checkbox" id="isAdmin" checked={isAdmin} onChange={handleAdminCheckboxChange} className="signup-input"
-                            />
-                            <label htmlFor="isAdmin">I am an Admin</label>
-                        </div> */
-                    }
-                    <button type="submit" className="signup-button">Sign Up</button>
+                    <input
+                        type="email"
+                        placeholder="Email"
+                        className="signup-input"
+                        required
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
+                    <PasswordInput
+                        placeholder="Password"
+                        showInfo={true}
+                        onChange={(e) => setPassword(e.target.value)}
+                        passwordError={passwordError}
+                    />
+                    <PasswordInput
+                        placeholder="Confirm password"
+                        value={confirmPassword}
+                        onChange={(e) => setConfirmPassword(e.target.value)}
+                        passwordError={passwordError}
+                    />
+                    <select
+                        className="signup-input"
+                        required
+                        value={role}
+                        onChange={(e) => setRole(e.target.value)}
+                    >
+                        <option value="">Select a Role</option>
+                        <option value="customer">Customer</option>
+                        <option value="admin">Admin</option>
+                    </select>
+                    <button type="submit" className="signup-button">
+                        Sign Up
+                    </button>
                 </form>
-                <Link className='switch' to="/login">Already a member? Login</Link>
+                <Link className="switch" to="/login">
+                    Already a member? Login
+                </Link>
             </div>
         </div>
     );
