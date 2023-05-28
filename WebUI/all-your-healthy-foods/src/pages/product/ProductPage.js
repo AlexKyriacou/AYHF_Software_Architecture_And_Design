d294331a2389899739a00138aea9d55593f3ce89
Enzo <enzopkp@gmail.com>
2023-05-28 18:11:47 +1000

    test

--- WebUI/all-your-healthy-foods/src/pages/product/ProductPage.js
+++ WebUI/all-your-healthy-foods/src/pages/product/ProductPage.js
@@ -12,7 +12,7 @@ import "./ProductPage.css";
 function ProductPage() {
     const { productName } = useParams();
     const product = productData.find(product => product.name === productName);
-
+    const productId = product.id;
     const { addToCart } = useContext(CartContext);
 
     const handleAddToCart = () => {
@@ -29,7 +29,7 @@ function ProductPage() {
     const handleDelete = async () => {
         try {
           // Make a DELETE request to the API endpoint to delete the product
-          const response = await fetch(`https://localhost:7269/products/${productName}`, {
+          const response = await fetch(`https://localhost:7269/products/${productId}`, {
             method: "DELETE",
           });
       
@@ -48,7 +48,7 @@ function ProductPage() {
       const handleSave = async () => {
         try {
           // Make a PUT request to the API endpoint to update the product
-          const response = await fetch(`https://localhost:7269/products/${productName}`, {
+          const response = await fetch(`https://localhost:7269/products/${productId}`, {
             method: "PUT",
             headers: {
               "Content-Type": "application/json",
@@ -68,62 +68,96 @@ function ProductPage() {
         }
       };
 
-    return (
+      return (
         <div className="product-overview-container">
-            <div className="overview-card">
-                <div className="image-container">
-                    <div className="back-link-container">
-                        <Link to="/" className="back-link">
-                            <FontAwesomeIcon icon={faArrowLeft} /> Back
-                        </Link>
-                    </div>
-                    <img className="large-view" src={product.image} alt={product.name} />
-                </div>
-                <div className="product-overview-details">
-                    <div className="product-info">
-                        <p className="product-name-desc">{product.name} {product.description}</p>
-                        <div className="product-rating">
-                            <Rating rate={product.rating} /> ({product.numRatings})
-                        </div>
-                        <p className="product-price">$ {(product.price).toFixed(2)}</p>
-                        <button className="add-to-cart-button" onClick={handleAddToCart}>Add to cart</button>
-                    </div>
-                    <details open>
-                        <summary>
-                            Description
-                        </summary>
-                        <p>{product.longDescription}</p>
-                    </details>
-                    <details open>
-                        <summary>
-                            Ingredients
-                        </summary>
-                        <p>{product.ingredients}</p>
-                    </details>
-                </div>
-                {loggedIn && user.role === "admin" && (
-                    <div className="admin-buttons">
-                        {!inEditMode && (
-                            <>
-                                <button className="primary-button" onClick={handleEdit}>
-                                    Edit
-                                </button>
-                                <button className="primary-button" onClick={handleDelete}>
-                                    Delete
-                                </button>
-                            </>
-                        )}
-                        {inEditMode && (
-                            <button className="primary-button" onClick={handleSave}>
-                                Save
-                            </button>
-                        )}
-                    </div>
-                )}
+          <div className="overview-card">
+            <div className="image-container">
+              <div className="back-link-container">
+                <Link to="/" className="back-link">
+                  <FontAwesomeIcon icon={faArrowLeft} /> Back
+                </Link>
+              </div>
+              <img className="large-view" src={product.image} alt={product.name} />
             </div>
+            <div className="product-overview-details">
+              <div className="product-info">
+                {inEditMode ? (
+                  <input
+                    type="text"
+                    name="name"
+                    value={editedProduct.name}
+                    onChange={handleInputChange}
+                  />
+                ) : (
+                  <p className="product-name-desc">
+                    {product.name} {product.description}
+                  </p>
+                )}
+                <div className="product-rating">
+                  <Rating rate={product.rating} /> ({product.numRatings})
+                </div>
+                {inEditMode ? (
+                  <input
+                    type="number"
+                    name="price"
+                    value={editedProduct.price}
+                    onChange={handleInputChange}
+                  />
+                ) : (
+                  <p className="product-price">$ {(product.price).toFixed(2)}</p>
+                )}
+                <button className="add-to-cart-button" onClick={handleAddToCart}>
+                  Add to cart
+                </button>
+              </div>
+              <details open>
+                <summary>Description</summary>
+                {inEditMode ? (
+                  <textarea
+                    name="longDescription"
+                    value={editedProduct.longDescription}
+                    onChange={handleInputChange}
+                  />
+                ) : (
+                  <p>{product.longDescription}</p>
+                )}
+              </details>
+              <details open>
+                <summary>Ingredients</summary>
+                {inEditMode ? (
+                  <textarea
+                    name="ingredients"
+                    value={editedProduct.ingredients}
+                    onChange={handleInputChange}
+                  />
+                ) : (
+                  <p>{product.ingredients}</p>
+                )}
+              </details>
+            </div>
+            {loggedIn && user.role === "admin" && (
+              <div className="admin-buttons">
+                {!inEditMode && (
+                  <>
+                    <button className="primary-button" onClick={handleEdit}>
+                      Edit
+                    </button>
+                    <button className="primary-button" onClick={handleDelete}>
+                      Delete
+                    </button>
+                  </>
+                )}
+                {inEditMode && (
+                  <button className="primary-button" onClick={handleSave}>
+                    Save
+                  </button>
+                )}
+              </div>
+            )}
+          </div>
         </div>
-    );
-}
+      );
+    }
 
 export default ProductPage;
 
