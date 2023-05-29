15c6ff8e8e4b551ffa3f8f526e13ca632ff9175b
Marella Morad <marella99mourad@gmail.com>
2023-05-30 04:53:27 +1000

    Fix shipping calc

--- WebUI/all-your-healthy-foods/src/pages/order/OrderSummary.js
+++ WebUI/all-your-healthy-foods/src/pages/order/OrderSummary.js
@@ -22,10 +22,17 @@ function calculateTotal(cartItems, promotionPercentage, shipping) {
     };
 }
 
-function OrderSummary({ extra, shipping }) {
+function OrderSummary({ extra, shipping, step }) {
     const { cartItems, sortedGroupedProducts } = useContext(CartContext);
     const promotionPercentage = 10; // Assuming a 10% promotion for this example
     const { subtotal, promotionAmount, total } = calculateTotal(Object.values(cartItems), promotionPercentage, shipping);
+
+    let shippingText;
+    if (step === "checkout") {
+        shippingText = "Calculated at next step";
+    } else if (step === "shipping" || step === "payment") {
+        shippingText = shipping ? `$${shipping}` : "Free";
+    }
 
     return (
         <div className="summary-card">
@@ -42,20 +49,20 @@ function OrderSummary({ extra, shipping }) {
             <hr />
             <div className="summary-row">
                 <span>Subtotal</span>
-                <span className="price">${subtotal}</span>
+                <span>${subtotal}</span>
             </div>
             <div className="summary-row">
                 <span>Promotion</span>
-                <span className="price">- ${promotionAmount}</span>
+                <span>- ${promotionAmount}</span>
             </div>
             <div className="summary-row">
                 <span>Shipping</span>
-                <span className="price">{shipping ? "+ $" + shipping : "Calculated at the Next step"}</span>
+                <span>{shippingText}</span>
             </div>
             <hr />
             <div className="summary-row">
                 <span>Total</span>
-                <span className="price">${total}</span>
+                <span>${total}</span>
             </div>
             <span className="extra">{extra}</span>
         </div>
