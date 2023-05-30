5dac6522c1e6ae24013f7868d8faa2bdf76904cf
Marella Morad <marella99mourad@gmail.com>
2023-05-31 03:22:56 +1000

    Calculate rating and num of reviews

--- WebUI/all-your-healthy-foods/src/pages/product/ProductCard.js
+++ WebUI/all-your-healthy-foods/src/pages/product/ProductCard.js
@@ -1,11 +1,24 @@
-import React, {useContext} from "react";
+import React, { useContext, useEffect, useState } from "react";
 import {Link} from "react-router-dom";
 import Rating from "../rating/Rating"
-import {CartContext} from "../../AppContext";
-import "./ProductCard.css";
+import {CartContext, ProductsContext} from "../../AppContext";
+import "./Product.css";
 
-function ProductCard({product}) {
-    const {addToCart} = useContext(CartContext);
+function ProductCard({ product }) {
+    const { getProductFeedbacks } = useContext(ProductsContext);
+    const { addToCart } = useContext(CartContext);
+
+    const [productFeedbacks, setProductFeedbacks] = useState([]);
+    const [averageRating, setAverageRating] = useState(0);
+
+    useEffect(() => {
+        const fetchFeedbacks = async () => {
+            const feedbackData = await getProductFeedbacks(product);
+            setProductFeedbacks(feedbackData.feedbacks);
+            setAverageRating(feedbackData.averageRating);
+        };
+        fetchFeedbacks();
+    }, [getProductFeedbacks, product]);
 
     const handleAddToCart = () => {
         addToCart(product);
@@ -15,14 +28,14 @@ function ProductCard({product}) {
         <div className="product-container">
             <div className="product-card">
                 <Link to={`/product/${product.name}`}>
-                    <img src={product.image} alt={product.name}/>
+                    <img src={product.image} alt={product.name} />
                 </Link>
                 <p className="product-name">{product.name}</p>
                 <p className="product-desc">{product.description}</p>
-                <div className="product-rating"><Rating rate={product.rating}/> 
-                <Link to={`/product/${product.name}/feedbacks`}>
-                    ({product.numRatings})
-                </Link>
+                <div className="product-rating"><Rating rate={averageRating} />
+                    <Link to={`/product/${product.name}/feedbacks`}>
+                        ({productFeedbacks.length})
+                    </Link>
                 </div>
                 <p className="product-price">$ {(product.price).toFixed(2)}</p>
                 <button className="add-to-cart-button" onClick={handleAddToCart}>Add to cart</button>
