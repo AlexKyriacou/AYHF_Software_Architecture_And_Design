b27e526197d90df2b00675b03fa15a1af22cee15
Julian Codespoti <julian.codespoti@myob.com>
2023-05-30 20:26:36 +1000

    add pwa functionality

--- WebUI/all-your-healthy-foods/src/index.js
+++ WebUI/all-your-healthy-foods/src/index.js
@@ -11,6 +11,21 @@ root.render(
     </React.StrictMode>
 );
 
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function() {
+        navigator.serviceWorker.register('/sw.js').then(function(registration) {
+            console.log('Worker registration successful', registration.scope);
+        }, function(err) {
+            console.log('Worker registration failed', err);
+        }).catch(function(err) {
+            console.log(err);
+        });
+    });
+} else {
+    console.log('Service Worker is not supported by browser.');
+}
+
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
